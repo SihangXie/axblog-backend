042f591100575f98943058527fd35f015d3a3304
SihangXie <shxie_scut@163.com>
2022-12-20 21:54:58 +0800

    创建项目

---
+++ axblog-framework/src/main/java/cn/axblog/Main.java
@@ -0,0 +1,15 @@
+package cn.axblog;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Package: cn.axblog Description： Author: Sihang Xie Date: Created in ${DATE} ${TIME} Company:
+ * Ocean University of China Copyright: Copyright (c) 2022 Version: 0.0.1 Modified By:
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
